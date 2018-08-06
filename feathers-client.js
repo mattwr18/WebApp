df370370cacfccd2d9081a2a9d4d837d9dc9c2cf
Robert Schäfer <git@roschaefer.de>
2018-08-06 16:58:51 +0200

    Configure auth service and reuse socket connection

--- feathers-client.js
+++ feathers-client.js
@@ -1,13 +1,36 @@
 import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
-import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
+import authentication from '@feathersjs/authentication-client'
+import urlHelper from '~/helpers/urls'
 import { CookieStorage } from 'cookie-storage'
 
-const socket = io('http://localhost:3030', {transports: ['websocket']})
+const authKey = 'feathers-jwt'
+const endpoint = urlHelper.buildEndpointURL(process.env.API_HOST, { port: process.env.API_PORT })
+let socket;
+if (process.env.ENV === 'production'){
+  socket = socketio(io(endpoint), { timeout: 20000 })
+  if (process.server) {
+    setTimeout(() => {
+      // close server connection as content was delivered already after 30 seconds at latest
+      try {
+        socket.close()
+      } catch (err) {
+        console.log(err)
+      }
+    }, 30000)
+  }
+} else {
+  socket = socketio(io(endpoint))
+}
 
-const feathersClient = feathers()
-  .configure(socketio(socket))
-  .configure(auth({ storage: new CookieStorage() }))
+let api = feathers()
+  .configure(socket)
+  .configure(authentication({
+    storage: new CookieStorage(),
+    storageKey: authKey,
+    cookie: authKey
+  }))
 
-export default feathersClient
+export { socket, endpoint, authKey };
+export default api
