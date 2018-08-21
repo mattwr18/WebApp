71a66acdf58e08df15802423856a2acecfb7c8fd
Robert Schäfer <git@roschaefer.de>
2018-08-22 01:48:11 +0200

    Implement rudimentary blacklist in user settings

--- pages/auth/settings/blacklist.vue
+++ pages/auth/settings/blacklist.vue
@@ -1,16 +1,32 @@
 <template>
+  <div class="info-text">
+    <h2 class="title is-3">
+      {{ $t('auth.settings.blacklist') }}
+    </h2>
+    <p class="subtitle is-6">{{ $t('auth.settings.blacklistSubtitle') }}</p>
+        <nuxt-link
+            v-for="(user, index) in blacklistedUsers"
+            :key="user._id"
+            :to="{ name: 'profile-slug', params: { slug: user.slug }}"
+            class="column user">
+            {{ user.name }}
+        </nuxt-link>
+  </div>
 </template>
 
 <script>
-
   export default {
     data() {
       return {
+        blacklistedUsers: [],
       }
     },
-    methods: {
-    },
-    computed: {
+    async asyncData ({store}) {
+      const currentUserSettings = store.getters['feathers-vuex-usersettings/current'];
+      const res = await store.dispatch('feathers-vuex-users/find', { query: {_id: { $in: currentUserSettings.blacklist } } } );
+      return {
+        blacklistedUsers: res.data
+      }
     }
   };
 </script>
