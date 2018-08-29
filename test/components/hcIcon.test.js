61c407cd1ed2ca99831a9efe3b18623f1abd9068
Matt Rider <mattwr18@gmail.com>
2018-08-29 18:29:06 -0300

    Add test dependencies and first component test

---
+++ test/components/hcIcon.test.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+import { shallow } from 'vue-test-utils';
+import Icon from '../../components/Global/Elements/Icon/Icon';
+
+test('It should render an `<i>`.', (t) => {
+  const wrapper = shallow(Icon);
+
+  t.true(wrapper.is('i'));
+});
