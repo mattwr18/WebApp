3b29daaa1991ad301f6602cb503bd91a5863ec95
Robert Schäfer <git@roschaefer.de>
2018-08-29 23:48:30 +0200

    Move tests in specific directories

--- test/components/Global/Element/Icon/hcIcon.test.js
+++ test/components/Global/Element/Icon/hcIcon.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import { shallow } from 'vue-test-utils';
-import Icon from '../../components/Global/Elements/Icon/Icon';
+import Icon from '../../../../../components/Global/Elements/Icon/Icon';
 
 test('It should render an `<i>`.', (t) => {
   const wrapper = shallow(Icon);
